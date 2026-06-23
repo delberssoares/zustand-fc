93364be530b08685c31ccda7b00adb78c957db73
delberss <delberss@hotmail.com>
2026-06-23 19:33:55 -0300

    feat: implementando todos os componentes criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { Header } from './components/Header'
 import { PlayerForm } from './components/PlayerForm'
 import { PlayerList } from './components/PlayerList'
+import { FormationForm } from './components/FormationForm'
+import { FormationField } from './components/FormationField'
 
 function App() {
   return (
-    <div style={{ maxWidth: 500, margin: '4rem auto', fontFamily: 'sans-serif' }}>
-      <h1>Zustand FC ⚽</h1>
-      <PlayerForm />
-      <PlayerList />
+    <div style={{ maxWidth: 600, margin: '4rem auto', fontFamily: 'sans-serif' }}>
+      <Header />
+
+      <section style={{ marginBottom: '2rem' }}>
+        <h2>Elenco</h2>
+        <PlayerForm />
+        <PlayerList />
+      </section>
+
+      <section>
+        <h2>Escalação</h2>
+        <FormationForm />
+        <FormationField />
+      </section>
     </div>
   )
 }
